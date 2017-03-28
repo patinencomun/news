ae1d50ad6f7f94bf0bfc474b8c2bef827a45cbfd
urbinapro <urbinapro@gmail.com>
2017-03-28 11:43:30 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,6 +1,16 @@
 $(function(){
     //cargar comunidades
-    
+    $.getJSON( "data/comunidades.json", function( jsonObject ) {
+        ponerComunidades( jsonObject );
+    });
+
+    function ponerComunidades(json){
+        $.each( json, function( i, comunidad ) {
+            //dos formas de hacer lo mismo, la segunda es más adecuada.
+            //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
+            $('#comunidades').append($('<option>', { value: comunidad.slug, text : comunidad.comunidad }));
+        }); 
+    }
 
     //onclick en verinfo
     $("#verinfo").click(function() {
