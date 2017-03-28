9d651a5d97d7a9159add0ab9b30587cc19de3e8a
urbinapro <urbinapro@gmail.com>
2017-03-28 11:49:51 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -9,6 +9,7 @@ $(function(){
             //dos formas de hacer lo mismo, la segunda es más adecuada.
             //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
             $('#comunidades').append($('<option>', { value: comunidad.slug, text : comunidad.comunidad }));
+            //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
         }); 
     }
 
